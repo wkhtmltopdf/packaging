4aa00e559e1cd9f92bfaa0cbe6f1ca2dd6b4197d
Denis Zatsepin <denis@zatsepin.me>
2020-09-20 17:07:57 +0200

    chore: remove unused files

--- scripts/aws-lambda.postcompile.sh
+++ scripts/aws-lambda.postcompile.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-libs="libjpeg libpng15 libXrender libfontconfig libfreetype libXext libX11 libxcb libXau"
+# We need only binary wkhtmltox files in lambda layer, so we delete all the others
+rm -rf /tgt/wkhtmltox/{include,share,lib}
 
+# We need to copy dependencies which are not presented in amazanolinux2 image
+libs="libjpeg libpng15 libXrender libfontconfig libfreetype libXext libX11 libxcb libXau"
+mkdir -p /tgt/wkhtmltox/lib
 for lib in $(echo $libs); do
     echo ".*${lib}\.so\.[0-9]{1,2}" | xargs find /usr/lib64 -regextype posix-awk -regex | xargs cp -t /tgt/wkhtmltox/lib/
 done
 
-rm -rf /tgt/wkhtmltox/{include,share}
-
+# To be able to generate pdf files we need a font. There is one (dejavu) in the complie docker image
 cp -r /usr/share/fonts /tgt/wkhtmltox/
 cat >/tgt/wkhtmltox/fonts/font.conf <<EOL
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
 <fontconfig>
-  <dir>/var/task/fonts/</dir>
-  <cachedir>/tmp/fonts-cache/</cachedir>
+  <dir>/var/task/fonts</dir>
+  <dir>/opt/fonts</dir>
+  <cachedir>/tmp/fonts-cache</cachedir>
   <config></config>
 </fontconfig>
 EOL
